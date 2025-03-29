ff5bee40a44a8e8ceb5e80786841054220fbd473
ggf0e <ilegnosov@gmail.com>
2025-03-29 20:30:02 +0300

    Initial Commit

---
+++ GameHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameHub
+//
+//  Created by Ivan Legnosov on 29.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GameHubTests/GameHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameHubTests.swift
+//  GameHubTests
+//
+//  Created by Ivan Legnosov on 29.03.2025.
+//
+
+import Testing
+@testable import GameHub
+
+struct GameHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
