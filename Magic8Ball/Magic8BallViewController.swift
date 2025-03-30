5e3f266c068c7ffaa83c9a0cc0ae815142e6a359
Alexuqe <hordiah1@gmail.com>
2025-03-30 17:59:45 +0300

    Update Magic8BallViewController.swift

--- Magic8Ball/Magic8BallViewController.swift
+++ Magic8Ball/Magic8BallViewController.swift
@@ -8,25 +8,17 @@
 
 import UIKit
 
-class Magic8BallViewController: UIViewController {
-    
+final class Magic8BallViewController: UIViewController {
+
     @IBOutlet weak var ballValue: UIImageView!
     @IBOutlet weak var textFieldValue: UITextField!
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-    
-    
+    private let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+
     @IBAction func tellButton(_ sender: UIButton) {
-        if textFieldValue.text! != "" {
+        if textFieldValue.text != nil {
             ballValue.image = ballArray[Int.random(in: 0...4)]
             textFieldValue.text = ""
         }
     }
-    
-    
-
-
-
 }
-
-
